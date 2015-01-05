46705f27f15041ae84fac57ef56265a80a8443a8
Andy Villiger <a.villiger@gmail.com>
2015-01-05 17:49:40 +0100

    added authentication and sample users

--- src/main/scala/proximety/services/ApiService.scala
+++ src/main/scala/proximety/services/ApiService.scala
@@ -3,9 +3,12 @@ package proximety.services
 import akka.util.Timeout
 import org.json4s.{DefaultFormats, Formats}
 import proximety.actors.ApiActor
-import proximety.data.Adapter
+import proximety.data.model.User
+import proximety.data.{Commands, Adapter}
+import proximety.util.Hash
 import spray.httpx.Json4sSupport
 import scala.concurrent.duration._
+import akka.pattern.ask
 
 trait ApiService extends TeapotService with AuthService with Json4sSupport with Adapter {
   self: ApiActor =>
@@ -13,6 +16,19 @@ trait ApiService extends TeapotService with AuthService with Json4sSupport with 
   implicit val executionContext = scala.concurrent.ExecutionContext.Implicits.global
   implicit val timeout = Timeout(5.seconds)
   implicit val json4sFormats: Formats = DefaultFormats
+
+  // PRELOAD INITIAL DATA
+  (Data.user ? Commands.Get("user1@proximety.ch")).mapTo[Option[User]].foreach {
+    case None =>
+      val user1 = User("user1@proximety.ch", Hash.sha1("1234"), admin = true)
+      val user2 = User("user2@proximety.ch", Hash.sha1("1234"), admin = false)
+      val user3 = User("user3@proximety.ch", Hash.sha1("1234"), admin = false)
+
+      Data.user ! Commands.Set(user1.email, user1)
+      Data.user ! Commands.Set(user2.email, user2)
+      Data.user ! Commands.Set(user3.email, user3)
+    case _ => // they already exist, we don't have to set them again
+  }
 
   val routes =
     TeapotService.routes ~
--- src/main/scala/proximety/services/AuthService.scala
+++ src/main/scala/proximety/services/AuthService.scala
@@ -1,8 +1,9 @@
 package proximety.services
 
 import proximety.data.Commands
-import proximety.data.model.Token
-import spray.routing.HttpService
+import proximety.data.model.{User, Token}
+import proximety.util.Hash
+import spray.routing.{AuthenticationFailedRejection, HttpService}
 import akka.pattern.ask
 
 case class EmailPassword(email: String, password: String)
@@ -19,7 +20,12 @@ trait AuthService extends HttpService {
           } ~
           post {
             entity(as[EmailPassword]) { data =>
-              complete((Data.token ? Commands.GenerateToken(data.email)).mapTo[Token])
+              onSuccess((Data.user ? Commands.Get(data.email)).mapTo[Option[User]]) {
+                case Some(user) if user.password == Hash.sha1(data.password) =>
+                  complete((Data.token ? Commands.GenerateToken(data.email)).mapTo[Token])
+                case _ =>
+                  reject(AuthenticationFailedRejection(AuthenticationFailedRejection.CredentialsRejected, List()))
+              }
             }
           }
         }
